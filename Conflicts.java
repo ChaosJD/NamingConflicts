9e62a167d9f00ddbed9b30e9b01fe21ae224f22a
Josef Sebastian Duschl <josef-sebastian.Duschl@adesso.de>
2022-01-12 17:13:30 +0100

    create Conflicts.java & Conflicts2.java

---
+++ Conflicts.java
@@ -0,0 +1,12 @@
+//One must be disabled !!!__|
+import java.util.*;       //|
+/* or */                  //|
+import java.sql.*;0       //|
+//__________________________|
+
+public class Conflicts{
+    public static void main(String[] args){
+        System.out.println("Naming Conflicts!!!");
+    }
+    Date date;
+}
---
+++ Conflicts2.java
@@ -0,0 +1,10 @@
+//This is bad too_______|
+import java.util.Date;//|
+import java.sql.Date; //|
+//______________________|
+public class Conflicts{
+    public static void main(String[] args){
+        System.out.println("Naming Conflicts!!!");
+    }
+    Date date;
+}
