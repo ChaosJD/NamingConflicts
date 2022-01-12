a55beba504d677a7c66124ada356600939ba5a8f
Josef Sebastian Duschl <josef-sebastian.Duschl@adesso.de>
2022-01-12 17:16:23 +0100

    create ConflictsSolution.java

---
+++ ConflictsSolution.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Conflicts{
+    public static void main(String[] args){
+        System.out.println("Solution1");
+    }
+    Date date;
+    java.sql.Date sqlDate;
+}
